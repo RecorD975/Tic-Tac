519117e4d478697abe14b8f8ee43f318a44bc41f
Adil <bekboev551@gmail.com>
2022-05-09 12:09:03 +0300

    +Interface

--- function.py
+++ function.py
@@ -1,21 +1,18 @@
+table = ['*', '*', '*', '*', '*', '*', '*', '*', '*']
+turner = True
+
+
 def win(table):
-    for i in range(len(table)):
-        if table[i][0] == table[i][1] == table[i][2] != '*':
+    for i in [0, 3, 6]:
+        if table[i] == table[i + 1] == table[i + 2] != '*':
             return 1
-        elif table[0][i] == table[1][i] == table[2][i] != '*':
+    for i in [0, 1, 2]:
+        if table[i] == table[i + 3] == table[i + 6] != '*':
             return 1
-    if table[0][0] == table[1][1] == table[2][2] != '*':
+    if table[0] == table[4] == table[8] != '*':
         return 1
-    elif table[0][2] == table[1][1] == table[2][0] != '*':
+    if table[2] == table[4] == table[6] != '*':
         return 1
-    for i in range(len(table)):
-        if '*' in table[i]:
-            return 0
+    if '*' in table:
+        return 0
     return -1
-
-
-def print_table(table):
-    for i in range(len(table)):
-        for j in range(len(table[i])):
-            print(table[i][j], end=" ")
-        print('\n', end='')
--- server.py
+++ server.py
@@ -1,56 +1,84 @@
 import function as func
 from socket import *
+from tkinter import *
+from threading import *
+from PIL import Image, ImageTk
+from functools import partial
 import time
+
+def table_clear():
+    for i in range(9):
+        func.table[i] = '*'
+
+
+def endgame():
+    user2.send("Хотите начать новую игру?".encode('utf-8'))
+    user1.send("Хотите начать новую игру?".encode('utf-8'))
+    data2 = user2.recv(1024)
+    data1 = user1.recv(1024)
+    msg2 = data2.decode('utf-8')
+    msg1 = data1.decode('utf-8')
+    if msg1 == msg2 == 'Да':
+        user2.send("Оба игрока согласились, рестарт ...".encode('utf-8'))
+        user1.send("Оба игрока согласились, рестарт ...".encode('utf-8'))
+        table_clear()
+        time.sleep(3)
+        return 1
+    else:
+        user2.send("Нет согласия от обоих игроков".encode('utf-8'))
+        user1.send("Нет согласия от обоих игроков".encode('utf-8'))
+        table_clear()
+        time.sleep(3)
+        return 0
 
 
 server = socket(AF_INET, SOCK_STREAM)
-server.bind(("0.0.0.0", 7000))
+server.bind(('localhost', 7000))
 server.listen(2)
 user1, address = server.accept()
+user1.send('1'.encode('utf-8'))
 user2, address = server.accept()
-
-table = [
-    ['*', '*', '*'],
-    ['*', '*', '*'],
-    ['*', '*', '*']
-]
-
-user2.send("Привет, ты зашел в игру крестики-нолики, ты будешь играть крестиком 'x'\
-\nПеред тобой игровое поле и его координаты\
- соответсвенно\n00 01 02\n10 11 12\n20 21 22".encode('utf-8'))
-user1.send("Привет, ты зашел в игру крестики-нолики, ты будешь играть ноликом '0'\
-\nПеред тобой игровое поле и его координаты\
- соответсвенно\n00 01 02\n10 11 12\n20 21 22".encode('utf-8'))
-time.sleep(0.5)
+user2.send('2'.encode('utf-8'))
+user2.send("Привет, ты зашел в игру крестики-нолики,\nты будешь играть крестиком 'x'\n".encode('utf-8'))
+user1.send("Привет, ты зашел в игру крестики-нолики,\nты будешь играть ноликом '0'\n".encode('utf-8'))
+time.sleep(5)
 while True:
-    if func.win(table):
-        time.sleep(0.5)
-        user2.send("Игра завершена победил '0'".encode('utf-8'))
-        user1.send("Игра завершена победил '0'".encode('utf-8'))
-        time.sleep(0.5)
-        break
-    user2.send("Введите координаты через пробел: ".encode('utf-8'))
+    user2.send("Ваш ход".encode('utf-8'))
     user1.send("Ваш соперник делает ход, ждем ...".encode('utf-8'))
     data = user2.recv(1024)
     user1.send(data)
     msg = data.decode('utf-8')
-    table[int(msg[0])][int(msg[1])] = 'x'
-    print(func.win(table))
-    if func.win(table) == 1:
+    func.table[int(msg)] = 'x'
+    if func.win(func.table) == 1:
         time.sleep(0.5)
         user2.send("Игра завершена победил 'x'".encode('utf-8'))
         user1.send("Игра завершена победил 'x'".encode('utf-8'))
-        time.sleep(0.5)
-        break
-    elif func.win(table) == -1:
+        time.sleep(2)
+        if endgame() == 1:
+            continue
+        else:
+            break
+    elif func.win(func.table) == -1:
         time.sleep(0.5)
         user2.send("Ничья".encode('utf-8'))
         user1.send("Ничья".encode('utf-8'))
-        time.sleep(0.5)
-        break
+        time.sleep(2)
+        if endgame() == 1:
+            continue
+        else:
+            break
     user2.send("Ваш соперник делает ход, ждем ...".encode('utf-8'))
-    user1.send("Введите координаты через пробел: ".encode('utf-8'))
+    user1.send("Ваш ход".encode('utf-8'))
     data = user1.recv(1024)
     user2.send(data)
     msg = data.decode('utf-8')
-    table[int(msg[0])][int(msg[1])] = '0'
+    func.table[int(msg)] = '0'
+    if func.win(func.table):
+        time.sleep(0.5)
+        user2.send("Игра завершена победил '0'".encode('utf-8'))
+        user1.send("Игра завершена победил '0'".encode('utf-8'))
+        time.sleep(2)
+        if endgame() == 1:
+            continue
+        else:
+            break
